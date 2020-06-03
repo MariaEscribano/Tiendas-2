48dd272d9783b52c35dd6feb6feb0104e82b217b
MariaEscribano <66299933+MariaEscribano@users.noreply.github.com>
2020-06-03 13:22:48 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GDH9gWmstGC5PMgTVzWvu3HmLkzyRe5UkSiZHZORzrc/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1PP6-Zr3cxfLycBNz0VrztElCyD8XsMBnXfZoJvJ3-sg/edit#gid=0';
